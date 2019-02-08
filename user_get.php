a6dd1593a641f426fc5747d990a1ed8de7053710
Gabriel Martins <gabrimartinsjuarez@gmail.com>
2019-02-08 16:29:04 -0200

    tratativas follow/unfollow

--- user_get.php
+++ user_get.php
@@ -13,7 +13,7 @@
     $db_object = new database();
     $link = $db_object->mysql_connect();
 
-    $sql = " SELECT * FROM users WHERE username LIKE '%$search_name%' AND id != $user_id ";
+    $sql = " SELECT u.*, f.* FROM users AS u LEFT JOIN followers as f ON (f.user_id = $user_id AND u.id = f.user_follow_id) WHERE u.username LIKE '%$search_name%' AND u.id != $user_id ";
 
     $id_result = mysqli_query($link, $sql);
 
@@ -23,8 +23,20 @@
                 echo '<strong>'.$user_data['username'].'</strong><small> - '.$user_data['email'].'</small>';
 
                 echo '<p class="list-group-item-text pull-right">';
-                    echo '<button type="button" id="btn_follow_'.$user_data['id'].'" class="btn btn-default btn_follow" data-user_id="'.$user_data['id'].'">Follow</button>';
-                    echo '<button type="button" id="btn_unfollow_'.$user_data['id'].'" class="btn btn-primary btn_unfollow" style="display:none" data-user_id="'.$user_data['id'].'">Unfollow</button>';
+                    $following = isset($user_data['user_follow_id']) && !empty($user_data['user_follow_id']) ? 'S' : 'N';
+
+                    $btn_follow_display = 'block';
+                    $btn_unfollow_display = 'block';
+
+                    if($following == 'N'){
+                        $btn_unfollow_display = 'none';
+                    } else {
+                        $btn_follow_display = 'none';
+                    }
+
+                    echo '<button type="button" id="btn_follow_'.$user_data['id'].'" class="btn btn-default btn_follow" style="display:'.$btn_follow_display.'" data-user_id="'.$user_data['id'].'">Follow</button>';
+
+                    echo '<button type="button" id="btn_unfollow_'.$user_data['id'].'" class="btn btn-primary btn_unfollow" style="display:'.$btn_unfollow_display.'" data-user_id="'.$user_data['id'].'">Unfollow</button>';
                 echo '</p>';
 
                 echo '<div class="clearfix"></div>';
